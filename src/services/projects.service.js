da2fe70af783658d8d2f2bcb1b5eece12453a90a
RJ Mohammad <rjmohammad@outlook.com>
2017-04-24 16:09:55 -0400

    view of all projects on page

--- src/services/projects.service.js
+++ src/services/projects.service.js
@@ -10,7 +10,70 @@ class Projects {
                 target_revenue_max: 50000000,
                 target_ebitda_min: -2000000,
                 target_ebitda_max: 10000000
-            }
+            },
+            {
+                headline: 'Funding Strategic Growth in the Restaurant Sector',
+                target_check_size_min: 2000000,
+                target_check_size_max: 10000000,
+                target_revenue_min: 0,
+                target_revenue_max: 50000000,
+                target_ebitda_min: -2000000,
+                target_ebitda_max: 10000000
+            },
+            {
+                headline: 'Funding Strategic Growth in the Restaurant Sector',
+                target_check_size_min: 2000000,
+                target_check_size_max: 10000000,
+                target_revenue_min: 0,
+                target_revenue_max: 50000000,
+                target_ebitda_min: -2000000,
+                target_ebitda_max: 10000000
+            },
+            {
+                headline: 'Funding Strategic Growth in the Restaurant Sector',
+                target_check_size_min: 2000000,
+                target_check_size_max: 10000000,
+                target_revenue_min: 0,
+                target_revenue_max: 50000000,
+                target_ebitda_min: -2000000,
+                target_ebitda_max: 10000000
+            },
+            {
+                headline: 'Funding Strategic Growth in the Restaurant Sector',
+                target_check_size_min: 2000000,
+                target_check_size_max: 10000000,
+                target_revenue_min: 0,
+                target_revenue_max: 50000000,
+                target_ebitda_min: -2000000,
+                target_ebitda_max: 10000000
+            },
+            {
+                headline: 'Funding Strategic Growth in the Restaurant Sector',
+                target_check_size_min: 2000000,
+                target_check_size_max: 10000000,
+                target_revenue_min: 0,
+                target_revenue_max: 50000000,
+                target_ebitda_min: -2000000,
+                target_ebitda_max: 10000000
+            },
+            {
+                headline: 'Funding Strategic Growth in the Restaurant Sector',
+                target_check_size_min: 2000000,
+                target_check_size_max: 10000000,
+                target_revenue_min: 0,
+                target_revenue_max: 50000000,
+                target_ebitda_min: -2000000,
+                target_ebitda_max: 10000000
+            },
+            {
+                headline: 'Funding Strategic Growth in the Restaurant Sector',
+                target_check_size_min: 2000000,
+                target_check_size_max: 10000000,
+                target_revenue_min: 0,
+                target_revenue_max: 50000000,
+                target_ebitda_min: -2000000,
+                target_ebitda_max: 10000000
+            },
         ];
     }
 }
