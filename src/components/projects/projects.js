475c825effdefad21d985b197125e858e5d8a92e
RJ Mohammad <rjmohammad@outlook.com>
2017-04-24 23:32:06 -0400

    wrote addProject function

--- src/components/projects/projects.js
+++ src/components/projects/projects.js
@@ -35,18 +35,35 @@ class ProjectCTRL {
       }
     })
   }
-  show(ev) {
-    console.log(ev)
-    this.$mdDialog.show(
-      this.$mdDialog.alert()
-        .parent(angular.element(document.querySelector('#popupContainer')))
-        .clickOutsideToClose(true)
-        .title('This is an alert title')
-        .textContent('You can specify some description text in here.')
-        .ariaLabel('Alert Dialog Demo')
-        .ok('Got it!')
-        .targetEvent(ev)
-    );
+  addProject() {
+    this.$mdDialog.show({
+      template: require('./dialogForm.html'),
+      parent: angular.element(document.body),
+      clickOutsideToClose: true,
+      fullscreen: false,
+      controller: ($scope, $mdDialog) => {
+        $scope.headline;
+        $scope.checkMin;
+        $scope.checkMax;
+        $scope.revenueMin;
+        $scope.revenueMax;
+        $scope.ebitdaMin;
+        $scope.ebitdaMax;
+        $scope.save = () => {
+          let obj = {
+            headline: $scope.headline,
+            target_check_size_min: $scope.checkMin,
+            target_check_size_max: $scope.checkMax,
+            target_revenue_min: $scope.revenueMin,
+            target_revenue_max: $scope.revenueMax,
+            target_ebitda_min: $scope.ebitdaMin,
+            target_ebitda_max: $scope.ebitdaMax,
+          };
+          this.projects.add(obj);
+          $mdDialog.hide();
+        }
+      }
+    })
   }
 }
 ProjectCTRL.$inject = ['projectsData', '$mdDialog']
