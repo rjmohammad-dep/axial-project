d895b29c20150afa03310d7cbc43519abe3b3d07
RJ Mohammad <rjmohammad@outlook.com>
2017-04-25 09:17:28 -0400

    more styling

--- src/directives/filter.js
+++ src/directives/filter.js
@@ -1,15 +1,16 @@
 export default class filter {
-  constructor() {
+  constructor($filter) {
     this.restrict = 'A',
       this.scope = false,
       this.require = 'ngModel'
-      this.link = (scope, element, attrs, ngModel) => {
-        console.log(attrs.value)
+      this.link = (scope, element, attrs, ngModel, $filter) => {
+        console.log($filter)
         element.bind('keyup', () => {
 
         })
         scope.$watch(attrs.ngModel, (v) => {
-          console.log(element.val().replace(/,/g, ''))
+          console.log(element.val)
+          element.val().replace(/,/g, '')
             element.val($filter('number')(value, false))
          ngModel.$setViewValue();
          ngModel.$render(); 
