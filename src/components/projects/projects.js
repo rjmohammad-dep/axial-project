934067612671c66aed9b7c718f24c4c4896c1ae0
RJ Mohammad <rjmohammad@outlook.com>
2017-04-24 23:09:42 -0400

    implemented edit form

--- src/components/projects/projects.js
+++ src/components/projects/projects.js
@@ -1,14 +1,29 @@
-
 class ProjectCTRL {
    constructor (projectsData, $mdDialog,  $mdSidenav) {
-     console.log( $mdDialog)
     this.projects = projectsData
     this.name = 'hello'
     this.$mdDialog = $mdDialog
   }
-
+  editProject (index) {
+    this.$mdDialog.show({
+      template: require('./dialogForm.html'),
+      parent: angular.element(document.body),
+      clickOutsideToClose: true,
+      fullscreen: false,
+      controller: ($scope) => {
+         let { headline, target_check_size_min, target_check_size_max, target_revenue_min, target_revenue_max, target_ebitda_min, target_ebitda_max } = this.projects.projects[index]
+        $scope.headline = headline
+        $scope.checkMin = target_check_size_min
+        $scope.checkMax = target_check_size_max
+        $scope.revenueMin = target_revenue_min
+        $scope.revenueMax = target_revenue_max
+        $scope.ebitdaMin = target_ebitda_min
+        $scope.ebitdaMax = target_ebitda_max
+      }
+    })
+  }
   show (ev) {
-    console.log(this.$mdDialog)
+    console.log(ev)
    this.$mdDialog.show(
      this.$mdDialog.alert()
         .parent(angular.element(document.querySelector('#popupContainer')))
@@ -23,6 +38,6 @@ class ProjectCTRL {
 }
 ProjectCTRL.$inject = ['projectsData', '$mdDialog']
 
-export default angular.module('ProjectCTRL', ['ngMaterial', "ngAnimate", "ngAria"])
+export default angular.module('ProjectCTRL', [])
 .controller('ProjectCTRL', ProjectCTRL)
 .name
